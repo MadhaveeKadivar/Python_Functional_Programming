3b8878f433cf5f2792a7639e3c1198635d2ec41b
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-16 11:26:25 +0530

    [Madhavee] Add : Coupon number program

---
+++ Coupon_Number.py
@@ -0,0 +1,34 @@
+'''
+    @Author: Madhavee Kadivar
+    @Date: 2022-05-14 09:48:15
+    @Last Modified by: Madhavee Kadivar
+    @Last Modified time: 2022-05-16 09:48:15
+    @Title : Coupon Number
+'''
+import random
+
+def random_numbers(length):
+    """ 
+        Description: 
+            This function is generating random numbers to cover all the distinct coupon number present in list
+        Parameter:
+            It takes number of distinct numbers
+        Return:
+            List of distinct coupon numbers
+    """
+    distinct_coupon_numbers = []
+    
+    while True: #This loop continue till all the distinct numbers will cover
+        random_number = random.randint(0,100) # Generating Random Number
+        if random_number in distinct_coupon_numbers: # checks if random number is already present in arry or not
+            continue
+        else:
+            distinct_coupon_numbers.append(random_number)# If present then remove this element from array
+        if len(distinct_coupon_numbers) == length:          
+            break            
+    return distinct_coupon_numbers
+
+total_distinct_numbers = int(input("\nHow many numbers you want to generate distinct coupon numbers : "))
+list_of_distinct_numbers=random_numbers(total_distinct_numbers)
+print(f"Distinct coupon number are : {list_of_distinct_numbers}")
+
