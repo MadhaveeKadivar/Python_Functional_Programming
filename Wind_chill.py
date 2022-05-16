94b8db512492bc28dc09af04f66310c9d8ece120
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-16 11:57:35 +0530

    [Madhavee] Add : Program to find wind chill by given temp and wind speed

--- Wind_chill.py
+++ Wind_chill.py
@@ -5,8 +5,9 @@
     @Last Modified time: 2022-05-15 06:36:02
     @Title : Wind chill
 '''
-# importing math module
+# importing math and sys module
 import math
+import sys
 def find_wind_chill(t, v): 
     """ 
         Description: 
@@ -19,10 +20,13 @@ def find_wind_chill(t, v):
     windchill = 35.74 + 0.6215*t + (0.4275*t - 35.75)*(math.pow(v,0.16))
     return windchill
 # Taking input from user
-temp = int(input("\nPlease enter temperature value in fahrenheit : "))
-wind_speed = int(input("\nPlease enter wind speed in miles per hour : "))
+# temp = int(input("\nPlease enter temperature value in fahrenheit : "))
+# wind_speed = int(input("\nPlease enter wind speed in miles per hour : "))
+#Command Line Argument
+temp = int(sys.argv[1])
+wind_speed = int(sys.argv[2])
 if temp <= 50 and (wind_speed <= 120 or wind_speed >= 3): #Checking condition
     windchill = find_wind_chill(temp, wind_speed) # Calling function
-    print("wind chill is: ", windchill)
+    print("\nwind chill is: ", windchill)
 else:
-    print("Required Temperature value is less than 50 and wind speed is less than 120 or greater than 3")
+    print("\nRequired Temperature value is less than 50 and wind speed is less than 120 or greater than 3")
